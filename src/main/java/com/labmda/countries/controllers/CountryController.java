8c22070755e5657f3903032e43d361690acbb558
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-07-11 09:43:46 -0700

    Add GET all countries, GET countries by first letter of name
    Rename misspelled CountryRepositories file

--- src/main/java/com/labmda/countries/controllers/CountryController.java
+++ src/main/java/com/labmda/countries/controllers/CountryController.java
@@ -1,4 +1,56 @@
 package com.labmda.countries.controllers;
 
+import com.labmda.countries.models.Country;
+import com.labmda.countries.repositories.CountryRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CountryController {
+    @Autowired
+    CountryRepositories countryid;
+
+    private List<Country> findCountries(List<Country> myList, CheckCountry lambda)
+    {
+        List <Country> tempList = new ArrayList<>();
+
+        for (Country c:myList)
+        {
+            if (lambda.test(c))
+            {
+                tempList.add(c);
+            }
+        }
+
+        return tempList;
+    }
+
+    // http://localhost:2019/names/all
+    @GetMapping(value = "/names/all", produces = {"application/json"})
+    public ResponseEntity<?> listAllNames()
+    {
+        List<Country> countryList = new ArrayList<>();
+        countryid.findAll().iterator().forEachRemaining(countryList::add);
+
+        return new ResponseEntity<>(countryList, HttpStatus.OK);
+    }
+
+    //http://localhost:2019/names/start/u
+    @GetMapping(value = "/names/start/{var}", produces = {"application/json"})
+    public ResponseEntity<?> listByStart(@PathVariable char var)
+    {
+        List<Country> countryList = new ArrayList<>();
+        countryid.findAll().iterator().forEachRemaining(countryList::add);
+
+        List<Country> returnList = findCountries(countryList, (e) -> Character.toLowerCase(e.getName().charAt(0)) == Character.toLowerCase(var));
+        return new ResponseEntity<>(returnList, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/labmda/countries/repositories/CountryRepositories.java
+++ src/main/java/com/labmda/countries/repositories/CountryRepositories.java
@@ -3,6 +3,6 @@ package com.labmda.countries.repositories;
 import com.labmda.countries.models.Country;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ContryRepository extends CrudRepository<Country, Long>
+public interface CountryRepositories extends CrudRepository<Country, Long>
 {
 }
